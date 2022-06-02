26084cd620ce7cb06f79db87d3ae8163fcd506b7
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-02 20:35:46 +0800

    改全卡池

---
+++ allkc.sh
@@ -0,0 +1,41 @@
+echo "Please wait..."
+cd ~
+cd 2.7
+cd data
+rm banners.json
+wget https://hoyvi.github.io/Banners.json
+clear
+echo "OK"
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
+##CZ_TWRP制作
