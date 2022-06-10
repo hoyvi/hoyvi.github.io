cc97914a5fa935e2bd9aa95019732d666b4aa61a
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-10 19:57:48 +0800

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,177 @@
+#!/usr/bin/env bash
+#####
+Y="\e[33m"
+G="\e[32m"
+R="\e[31m"
+white="\e[0m"
+#####
+#########czawa%制作 QQ：18401%79726#########
+#########czawa制作 QQ：%1840179%726#########
+#########czawa制作 QQ：1%840179726#########
+#########czawa制%作 QQ：18%40179726#########
+#########czawa制作 QQ：%%1840179726#########
+#########czawa制作 QQ：184017%9726#########
+cd /sd
+#########czawa制作 QQ：1%840179726#########
+mkdir Banners_backup
+cd ~
+clear
+#########czawa制作 QQ：1840179726#########
+echo -e "\e[33m重复克隆将会把原来克隆的卡池替换！重复克隆将会把原来克隆的卡池替换!\e[0m"
+echo -e "\e[31m重复克隆将会把原来克隆的卡池替换！重复克隆将会把原来克隆的卡池替换!\e[0m"
+echo -e "\e[32m重复克隆将会把原来克隆的卡池替换！重复克隆将会把原来克隆的卡池替换!\e[0m"
+echo -e 欢迎使用"\e[31mczawa\e[0m\e[31m制作\e[0m"的修改卡池!
+#########czawa制作 QQ：1840179726#########
+echo -e "\e[33m注意！克隆保存的文件目录是/sdcard/Banners_backup文件夹内\e[0m"
+echo -e "\e[33m----------------\nB站czawa制作 QQ：1840179726\nB站czawa制作 QQ：1840179726\n----------------\e[0m"
+echo -e "\e[32m使用方法：\e[0m"
+echo -e 您输入的是：“"\e[33mbackup\e[0m"”
+echo -e "\e[0m终端输出：\e[0m"
+#########czawa制作 QQ：184017%9726#########
+echo -e "\e[33m正在执行克隆备份操作...\e[0m"
+echo -e "\e[32mOK\e[0m"
+echo #
+
+
+
+
+
+echo -e 当前版本:"\e[33m0.1-rc\e[31m7\e[0m"
+echo -e 更新日志："\e[31m\n[更新]\e[32m美化\e[0m"
+
+
+
+
+
+
+#########=cza/:wa*制作 QQ：1/84017==9726#########
+echo ##############
+#########czawa%%制作 QQ%：1840179726#########
+#########czawa制作 QQ：1%8401%79726#%########
+echo -e "\e[33m**查询卡池**\e[0m"
+echo -e "\e[32msearch)查询卡池\e[0m"
+echo -e "\e[33m**克隆**\e[0m"
+echo -e "\e[32mbackup)克隆卡池到/sdcard/Banners_backup\e[0m"
+#########czawa制作 QQ：18%40179726#########
+echo -e "\e[33m**自定义替换卡池**\e[0m"
+echo -e "\e[32mzdy)自定义替换卡池\e[0m"
+#########czawa制作 QQ：1840179726#########
+echo -e "\e[31m注意事项：\n将/sdcard下的Banners.json移动到开服端\n替换完成后,请在控制台输入reload\n如果您没有开服务器,那在你下一次开服务器时自动生效!\e[0m"
+echo -e "\e[33m**-----换卡池-----**\e[0m"
+echo -e "\e[32m1.0)\e[33m1.0卡池//常驻祈愿池//温蒂up//阿莫斯.风鹰//可莉up//狼末.四风\e[0m"
+echo -e "\e[32m1.1)\e[33m1.1卡池//天空之翼//官鸿//达达利亚up//常驻等\e[0m"
+echo -e "\e[32m1.2)\e[33m1.2卡池//阿贝多up//甘雨up//常驻//.等等\e[0m"
+echo -e "\e[32m1.3)\e[33m1.3卡池//魈//刻晴//胡桃//武器x2//常驻\e[0m"
+echo -e "\e[32m1.4)\e[33m1.4卡池//温蒂复刻up//达达利亚复刻up\e[0m"
+echo -e "\e[32m1.5)\e[33m1.5卡池//钟离//优菈\e[0m"
+echo -e "\e[32m制作中...\e[33m\e[0m"
+
+read -r -p "
+*********************
+您输入的是：" input
+#########czawa制作 QQ：1840179726#%########
+#########cz%awa%制作 QQ：%1840%179726#########
+#########czawa制%@作 QQ：1840%179726#########
+#########czaw%a制作 QQ：184%0179726#########
+#########czawa制作 QQ：1840179726#%########
+#########czawa%制%作 QQ：%%1840179726#########
+#########cza@wa制作 @QQ@：1@84%%017@9726#########
+#########czawa制@作 QQ：1840179@726#########
+case $input in
+search)
+echo -e "\e[33m正在查询您当前卡池数据...\e[0m"
+#########czawa制作 QQ：1840179726#########
+sleep 3s 
+echo -e "\e[33m以下是您当前卡池数据\e[0m"
+cat ~/2.7/data/Banners.json 
+#########czawa制作 QQ：1840179726#########
+echo  
+echo -e "\e[33m-------分割线-------\e[0m"
+#########czawa制作 QQ：1840179726#########
+echo -e "\e[33m查询完成!\e[0m"
+echo -e 感谢您对"\e[33mczawa\e[0m"的支持!
+;;
+backup)echo 正在执行克隆备份操作... && sleep 5s && cp ~/2.7/data/Banners.json /sd/Banners_backup && cd ~ && echo OK! && echo 您备份的卡池复制到/sdcard/Banners_backup文件夹中 ;;
+1.0)echo -e "\e[33m正在调用1.0卡池数据...\e[0m"
+sleep 1.5 
+echo -e 1.0卡池包含"\e[33m//常驻祈愿池//温蒂up//阿莫斯.风鹰//可莉up//狼末.四风\e[0m"
+echo 正在替换... 
+#########czawa制作 QQ：1840179726#########
+######czawa制作 QQ：1840179#czawa#726#
+sleep 3s 
+cp /sbin/kachi/1.0.json ~/2.7/data && cd ~/2.7/data && mv 1.0.json Banners.json && cat ~/2.7/data/Banners.json 
+echo -e "\e[32m恭喜你，替换完成！\e[0m"
+cd ~ ;;
+1.1)
+echo -e "\e[33m正在调用1.1卡池数据...\e[0m"
+#########czawa制作 QQ：1840179726#########
+sleep 1.5 
+echo -e 1.1卡池包含"\e[33m//天空之翼，锁，笛剑，雨裁，昭心，宫藏，西风\n//官鸿，蜈蚣，龙吟，钟，西风，西风，闸里\n//达达利亚up\n//常驻\e[0m"
+echo 正在替换... 
+sleep 3s 
+cp /sbin/kachi/1.1.json ~/2.7/data && cd ~/2.7/data && mv 1.1.json Banners.json && cat ~/2.7/data/Banners.json 
+echo -e "\e[32m恭喜你，替换完成！\e[0m"
+cd ~ ;;
+#########czawa制作 QQ：1840179726#########
+1.2)
+echo -e "\e[33m正在调用1.2卡池数据...\e[0m"
+#########czawa制作 QQ：1840179726#########
+sleep 1.5 
+echo -e 1.2卡池包含"\e[33m阿贝多up/甘雨up\e[0m"
+echo 正在替换... 
+sleep 3s 
+cp /sbin/kachi/1.2.json ~/2.7/data 
+cd ~/2.7/data 
+mv 1.2.json Banners.json 
+cat ~/2.7/data/Banners.json 
+echo -e "\e[32m\n恭喜你，替换完成！\e[0m"
+cd ~
+;;
+1.3)
+#########czawa制作 QQ：1840179726#########
+echo -e "\e[33m正在调用1.3卡池数据...\e[0m"
+#########czawa制作 QQ：1840179726#########
+sleep 1.5 
+echo -e 1.3卡池包含"\e[33m//魈//刻晴//胡桃//武器x2//常驻\e[0m"
+echo 正在替换... 
+sleep 3s 
+cp /sbin/kachi/1.3.json ~/2.7/data 
+cd ~/2.7/data 
+mv 1.3.json Banners.json 
+cat ~/2.7/data/Banners.json 
+echo -e "\e[32m\n恭喜你，替换完成！\e[0m"
+cd ~
+;;
+#########czaw%a制作 %QQ：184017%9726#########
+1.4)echo -e "\e[33m正在调用1.4卡池数据...\e[0m"
+#########czawa制作 QQ：1840179726#########
+sleep 1.5 
+echo -e 1.4卡池包含"\e[33m达达利亚复刻up//温蒂复刻up\e[0m"
+echo 正在替换... 
+sleep 3s 
+cp /sbin/kachi/1.4.json ~/2.7/data 
+cd ~/2.7/data 
+mv 1.4.json Banners.json 
+cat ~/2.7/data/Banners.json 
+echo -e "\e[32m\n恭喜你，替换完成！\e[0m"
+cd ~
+;;
+1.5)
+echo -e "\e[33m正在调用1.5卡池数据...\e[0m"
+#########czawa制作 QQ：1840179726#########
+sleep 1.5 
+echo -e 1.5卡池包含"\e[33m//钟离//优菈\e[0m"
+echo 正在替换... 
+sleep 3s 
+cp /sbin/kachi/1.5.json ~/2.7/data 
+cd ~/2.7/data 
+mv 1.5.json Banners.json 
+cat ~/2.7/data/Banners.json 
+echo -e "\e[32m\n恭喜你，替换完成！\e[0m"
+cd ~
+;;
+#########czawa制作 QQ：1840179726#########1.4
+_)sh 执行.sh ;;
+zdy)echo 正在查询/sdcard下的Banners.json.... && cat /sd/Banners.json && echo  && echo 当前内容是否为您想要替换的Banners.json? && echo 5s后替换.. && sleep 5s && cp /sd/Banners.json ~/2.7/data/ && echo '********' && cat ~/2.7/data/Banners.json && echo  && echo '********' && echo 替换完成! && echo 请检查内容是否为您要替换的卡池... ;;
+exit)exit ;;
+esac
