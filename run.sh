ccef46958be7d05fa0bbc5a09c4e7f18633f3528
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-10 19:55:18 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,8 +1,83 @@
-sleep 2
-echo 只能用我的恢复包使用此命令
-echo 3秒后开启服务器...
-echo 关闭服务器 先输入stop
-echo 然后状态栏点击Termux的通知里有stop~
-sleep 3.01s
-cd 2.7
-sudo mkdir /usr/db;sudo mongod --dbpath /usr/db --bind_ip 127.0.0.1 & mitmdump -k -s proxy.py --ssl-insecure -p 20000 & .//java -jar grasscutter.jar
+cd ~
+echo 感谢您使用CZ_TWRP的恢复包
+echo #
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo QQ号：1840179726
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+sleep 1.01s 
+echo 感谢您使用CZ_TWRP的恢复包
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo QQ号：1840179726
+sleep 3
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#现名字已改为czawa
+#现名字已改为czawa
+clear 
+sleep 1
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo 输入/gm会发送给您一份邮件
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo 就可以打开网页命令控制台
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo 3秒后开启服务器
+sleep 3
+cd 2.7 
+#感谢您使用CZ_TWRP B站名字:czawa的恢复包 群号：171062930 QQ号：1840179726
+sudo mkdir /usr/db;sudo mongod --dbpath /usr/db --bind_ip 127.0.0.1 & ~/java -jar ~/2.7/gc-1.2.0.jar
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
