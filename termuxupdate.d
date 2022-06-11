aefce8c4904c974ab452109b2eb0b42545176a95
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-11 18:38:19 +0800

    Add files via upload

--- termuxupdate.d
+++ termuxupdate.d
@@ -3,7 +3,7 @@ echo 安装依赖中...
 apt install wget -y
 wget hoyvi.github.io/start_command.sh
 ###czawa制作
-mv start_command.sh /data/data/com.termux/files/home/storage/shared/
+mv start_command.sh ~/.xinhao_history/start_command.sh
 echo [1/1]已完成...
 echo 即将进入下一环节
 debian
