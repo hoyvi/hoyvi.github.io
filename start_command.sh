66182925a871086316832bf4b46735c5452d1c60
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-11 17:59:32 +0800

    启动脚本更新
    1.0

---
+++ start_command.sh
@@ -0,0 +1,40 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#XINHAO_HAN
+#
+#
+#     version 1.0
+#
+#
+#
+#
+#  有什么问题?
+#
+#  目前运作，不是APP启动后只执行1次，而是您创建一次会话窗口，开机命令就会执行一次
+#
+#  如何解决
+#
+#  该问题是指每创建一个bash[会话窗口]从而会默认与bash同时启动,它不是一个BUG,而是您使用当中的特殊需求
+#
+#
+#开机启动命令脚本
+clear
+#版本 1.0 ,所有的启动脚本命令可加入此sh中
+
+
+#--------------------------------------------
+#历史命令启动sh
+#cd ~ > /dev/null&
+#chmod 777 .xinhao_history/history.sh > /dev/null &
+#./.xinhao_history/history.sh > /dev/null &
+#--------------------------------------------
+
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+echo -e "\e[33m请输入命令：czawa 启动菜单！\e[0m"
+debian
