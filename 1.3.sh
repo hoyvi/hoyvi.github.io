05ecbd5692301a1c6167a6eda698d6719926122c
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-12 21:37:57 +0800

    Add files via upload

---
+++ 1.3.sh
@@ -0,0 +1,83 @@
+cd ~
+echo 感谢您使用CZ_TWRP的恢复包
+echo #
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo QQ号：1840179726
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+sleep 1.01s 
+echo 感谢您使用CZ_TWRP的恢复包
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo QQ号：1840179726
+sleep 3
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#现名字已改为czawa
+#现名字已改为czawa
+clear 
+sleep 1
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo 输入/gm会发送给您一份邮件
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo 就可以打开网页命令控制台
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+echo 3秒后开启服务器
+sleep 3
+cd 2.7 
+#感谢您使用CZ_TWRP B站名字:czawa的恢复包 群号：171062930 QQ号：1840179726
+sudo mkdir /usr/db;sudo mongod --dbpath /usr/db --bind_ip 127.0.0.1 & ~/java -jar ~/2.7/1.2.1dev.jar
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
+#CZ_TWRP B站名字:czawa
+#现名字已改为czawa
