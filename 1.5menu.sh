2eb3f02220fe618717c838ee013b33e25031ea45
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-19 20:42:36 +0800

    Update 1.5menu.sh

--- 1.5menu.sh
+++ 1.5menu.sh
@@ -16,10 +16,14 @@ case $input in
 1)
    start=$(whiptail --title "czawa-Update" --menu "      欢迎使用czawa制作的Menu，选择一个选项以开始\n\n当前已有最新版本,是否更新？\n版本号：1.2.1-dev" 20 50 9 \
 	"1" "更新 (Upgrade)" \
-	"0" "不更新并启动服务器 (start server)"  3>&1 1>&2 2>&3)
+        "2" "启动含有代理的服务器 (Start Proxy Server)" \
+	"0" "不更新并启动服务器 (Start Server)"  3>&1 1>&2 2>&3)
   case $start in
   1)
   echo 脚本未完善
+  ;;
+  2)
+  bash -c "$(curl -L https://hoyvi.github.io/onproxyserver.z)" ;
   ;;
   0)
   bash -c "$(curl -L https://hoyvi.github.io/start)" ;
