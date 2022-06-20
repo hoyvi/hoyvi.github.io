e44fd7466197e3ad1ba7c35d8f343308c55f48d5
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-20 20:40:11 +0800

    Update 1.6menu.sh

--- 1.6menu.sh
+++ 1.6menu.sh
@@ -6,11 +6,18 @@ R="\e[31m"
 E="\e[0m"
 #####
 
-input=$(whiptail --title "czawa Menu" --menu "      欢迎使用czawa制作的Menu，选择一个选项以开始" 20 50 9 \
+input=$(whiptail --title "czawa Menu" --menu "      欢迎使用czawa制作的Menu，选择一个选项以开始" 20 57 9 \
 	"1" "启动服务器" \
+        "2" "删除日志文件(最好每天一次,闲的没事点点)"
 	"0" "退出"  3>&1 1>&2 2>&3)
 
 case $input in
+2)
+clear
+rm -rf /grasscutter/logs
+echo 删除完成！即将返回菜单
+sleep 3
+czawa
 0)bash -c "$(curl -L https://hoyvi.github.io/czawa)" 
 ;;
 1)
