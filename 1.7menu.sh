6369ae6438df193a669390471ee7a6ce79729e55
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-21 23:06:57 +0800

    Update 1.7menu.sh

--- 1.7menu.sh
+++ 1.7menu.sh
@@ -1,4 +1,4 @@
 cd ~
-wgrt https://hoyvi.github.io/ShopChest.json & clear & clear & clear
+wget https://hoyvi.github.io/ShopChest.json & clear & clear & clear
 mv ShopChest.json /grasscutter/data & clear
 bash -c "$(curl -L https://hoyvi.github.io/update)"
