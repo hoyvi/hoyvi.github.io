bbba72b59b136a1cef2f697ce1721e9073bb56b2
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-06-25 18:19:24 +0800

    Update change.sh

--- change.sh
+++ change.sh
@@ -6,14 +6,14 @@ R="\e[31m"
 E="\e[0m"
 #####
 cd ~
-echo -e "\e[33m当前正在执行替换操作,请勿操作终端"
+echo -e "\e[0m"
 sleep 2
 input=$(whiptail --title "问题1" --menu "联系QQ：1840179726 \n  请问你家网络支持ipv6吗？不支持请访问\n     test-ipv6.cz测试  浏览器打开即可 \n " 20 50 4 \
       "1" "通过github.com下载(网速很慢)" \
       "2" "通过czawa自搭下载站下载(问题多,无法下载联系我)" \
       "3" "我TM不下了" \
       "0" "[必须]刚刚下载的失败,请选择我!  "3>&1 1>&2 2>&3)
-case $start in
+case $input in
 1)
 echo -e "\e[31m下载中请勿断网！否则会出现问题\e[0m"
 wget https://github.com/hoyvi/hoyvi.github.io/releases/download/gras/grasscutter-1.2.2-dev.jar
