31298389ec832d3c9a23128669c8b3420fe335d7
hoyvi <88294533+hoyvi@users.noreply.github.com>
2022-07-04 07:30:55 +0800

    Delete Rchange.sh

--- Rchange.sh
+++
@@ -1,8 +0,0 @@
-input=$(whiptail --title "Warning!警告!" --menu "      当前正在执行替换核心为割草机官方最新jar!\n人工更新可能稍有延迟\n作者QQ:1840179726”\n      作者：czawa" 20 50 4 \
-      "1" "我要更新" \
-      "2" "我按错了" 3>&1 1>&2 2>&3)
-        case $input in
-        1)
-        bash -c "$(curl -L https://hoyvi.github.io/change.sh)" ;;
-        2)czawa ;;
-        esac
